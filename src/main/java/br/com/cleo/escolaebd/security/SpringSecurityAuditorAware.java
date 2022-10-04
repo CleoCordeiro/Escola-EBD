2106a133801ea8620bc3db053b5f283317e07fb7
Cleo <Cleo-maia@outlook.com.br>
2022-10-04 10:30:27 -0300

    Initial version of EscolaEBD generated by generator-jhipster@7.9.3

---
+++ src/main/java/br/com/cleo/escolaebd/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package br.com.cleo.escolaebd.security;
+
+import br.com.cleo.escolaebd.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
---
+++ src/main/java/br/com/cleo/escolaebd/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package br.com.cleo.escolaebd.service.dto;
